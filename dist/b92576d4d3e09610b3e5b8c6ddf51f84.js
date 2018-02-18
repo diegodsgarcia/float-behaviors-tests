a0e01856c19f742bbf268a4cd06ac5b17a24de10
Diego Garcia <diego.ds.garcia@gmail.com>
2018-02-18 15:24:17 -0300

    Add readm.md and add all cases

--- dist/b92576d4d3e09610b3e5b8c6ddf51f84.js
+++ dist/b92576d4d3e09610b3e5b8c6ddf51f84.js
@@ -138,7 +138,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":4}],6:[function(require,module,exports) {
+},{"_css_loader":4}],22:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -261,5 +261,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[6])
+},{}]},{},[22])
 //# sourceMappingURL=/dist/b92576d4d3e09610b3e5b8c6ddf51f84.map
---
+++ readm.md
@@ -0,0 +1,13 @@
+# Float tests
+This is just a behaviors of float in somes displays
+
+The appication uses parcejs
+
+
+```js
+npm install -g parcel-bundler
+```
+
+```js
+parcel index.html
+```
